4e0626b18a45a4551642b09e959821a55cf205d7
Andrew Ross <andrew@vermonster.com>
2015-03-26 17:34:56 -0400

    indexes and years

--- add_indexes_and_years_to_cycling_data.rb
+++ add_indexes_and_years_to_cycling_data.rb
@@ -4,13 +4,15 @@ require 'json'
 
 input = CSV.parse(File.read('./flattened_cycling_data.csv'), headers: true)
 
-headers = ['Latitude', 'Longitude', 'Activity Type', 'Datetime', 'Route ID', 'Path Index']
+headers = ['Latitude', 'Longitude', 'Activity Type', 'Datetime', 'Route ID', 'Path Index', 'Year']
 
-CSV.open('./flattened_cycling_data_with_indexes.csv', 'w', headers: headers, write_headers: true) do |output|
+CSV.open('./flattened_cycling_data_with_indexes_and_years.csv', 'w', headers: headers, write_headers: true) do |output|
   input.group_by { |row| row['Route ID'] }.each do |route_id, rows|
     rows.each_with_index do |row, i|
       row['Path Index'] = i + 1
       row['Activity Type'] = 'Cycling'
+      time = Time.parse(row['Datetime'])
+      row['Year'] = time.year
       output << row.values_at(*headers)
     end
   end
