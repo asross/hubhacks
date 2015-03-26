8e65512f6a559521ebb872776ea47a59d01fb0ce
Andrew Ross <andrew@vermonster.com>
2015-03-26 16:34:12 -0400

    fix bug in cycling path index-adding

--- add_indexes_to_cycling_data.rb
+++ add_indexes_to_cycling_data.rb
@@ -4,11 +4,14 @@ require 'json'
 
 input = CSV.parse(File.read('./flattened_cycling_data.csv'), headers: true)
 
-CSV.open('./flattened_cycling_data_with_indexes.csv', 'w', headers: ['Latitude', 'Longitude', 'Activity Type', 'Datetime', 'Route ID', 'Path Index'], write_headers: true) do |output|
+headers = ['Latitude', 'Longitude', 'Activity Type', 'Datetime', 'Route ID', 'Path Index']
+
+CSV.open('./flattened_cycling_data_with_indexes.csv', 'w', headers: headers, write_headers: true) do |output|
   input.group_by { |row| row['Route ID'] }.each do |route_id, rows|
     rows.each_with_index do |row, i|
       row['Path Index'] = i + 1
-      output << row
+      row['Activity Type'] = 'Cycling'
+      output << row.values_at(*headers)
     end
   end
 end
